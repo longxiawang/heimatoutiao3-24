1edcea963dad94169ededce643f4943fb7cd65c0
longxiawang <798764973@qq.com>
2022-03-26 23:12:23 +0800

    完成了列表上拉加载更多，以及改为ListData组件请求数据

--- src/components/ListData.vue
+++ src/components/ListData.vue
@@ -5,9 +5,10 @@
       :finished="finished"
       finished-text="没有更多了"
       @load="onLoad"
+      :immediate-check='false'
     >
       <ItemData
-        v-for="item in NewsList"
+        v-for="item in NewList"
         :key="item.art_id"
         :NewsItem="item"
       ></ItemData>
@@ -18,22 +19,62 @@
 <script>
 // 导入单独列模板结构组件
 import ItemData from './ItemData.vue'
+// 导入请求数据列表的组件
+import { GetNewsList } from '../api/index'
+// 导入获得token值的封装函数组件
+import { GetToken } from '../utils/tokens'
+
 export default {
   components: {
     ItemData
   },
-  props: ['NewsList'], // 新闻列表推荐数据，父MyHome组件传过来的
+
+  props: ['NewsList', 'name'], // 新闻列表推荐数据，父MyHome组件传过来的，name为本列表容器频道分类的id值，比如我这个列表是html类频道，则我的id就是1.比如我的分类是css频道，则我的id则为4...，这个数据实在循环生成列表分类的时候，父组件传过来的
+
   data () {
     return {
       list: [],
       loading: false,
-      finished: false
+      finished: false,
+      NewList: [],
+      TheTime: new Date().getTime()
     }
   },
-  methods: {
-  // 上拉加载更多的函数
-    onLoad () {
 
+  // 每个List页面容器组件，数据代理完成勾子节点上，去发送请求拿到自己该类频道的列表数据，这里this.name 就是父在循环生成本容器的时候，传递过来的每个分类频道的id值，拿这个ID值去请求数据，则能获得本分类频道的数据列表
+  async created () {
+    const NewsList = await GetNewsList(
+      GetToken('token'),
+      this.name,
+      // 初始打开频道分类页面的请求时间戳，为本地最新时间戳
+      this.TheTime
+    )
+    // 返回的数据中抽离出文章列表集合赋值给数据库的数组内
+    this.NewList = NewsList.data.data.results
+    // 把每次请求回来的一个 请求前一页历史数据的时间戳,的一个参数，赋值给数据库里面的TheTime，
+    this.TheTime = NewsList.data.data.pre_timestamp
+  },
+
+  methods: {
+    // 上拉加载更多的函数
+    async onLoad () {
+      const OldNewsList = await GetNewsList(
+        GetToken('token'),
+        this.name,
+        // 上拉加载更多的时候，时间戳就为上一次的服务器返回的时间戳
+        this.TheTime
+      )
+      // 每次请求完加一个判断，如果请求回来的数据长度为0，则说明没数据了，那么久按照组件文档说明，设置finished为true，并且返回即可
+      if (OldNewsList.data.data.results.length === 0) {
+        this.finished = true
+        return false
+      }
+      // 把返回来的新数据+原来的列表数据合并起来，以此来 循环单列结构模板组件ItemData
+      this.NewList = [...this.NewList, ...OldNewsList.data.data.results]
+      // 继续覆盖时间戳
+      this.TheTime = OldNewsList.data.data.pre_timestamp
+
+      this.loading = false
     }
   }
 }
--- src/views/MyHome/MyHome.vue
+++ src/views/MyHome/MyHome.vue
@@ -12,7 +12,7 @@
     </van-nav-bar>
     <!-- Tab栏分区切换区域  sticky：粘性定位，offset-top=1.226666rem 行内样式插件无法计算手动46/37.5-->
     <div class="tab" >
-      <van-tabs v-model="activeName" sticky offset-top=1.226666rem @click="onClick" >
+      <van-tabs v-model="activeName" sticky offset-top=1.226666rem>
         <!-- 循环生成vant Tab标签导航栏组件 -->
         <!-- 每一个Vant Tab导航栏标签组件内，在放入内容列表数据的容纳组件 -->
         <!-- ListData为每一列数据结构列表的容纳组件，该组件内放置着组件ItemData，为单列结构模板组件 -->
@@ -24,7 +24,7 @@
 
 <script>
 // 导入获得用户关注频道的请求函数组件
-import { GetVipListApi, GetNewsList } from '../../api/index'
+import { GetVipListApi } from '../../api/index'
 // 发送请求需要提交token，导入token函数组件
 import { GetToken } from '../../utils/tokens'
 // 导入内容列表数据的容纳组件
@@ -44,11 +44,6 @@ export default {
     ListData
   },
   methods: {
-    // 点击每一个频道列表的时候，都触发一个函数，请求一次频道列表数据
-    async onClick () {
-      const NewsList = await GetNewsList(GetToken('token'), this.activeName, new Date().getTime())
-      this.NewsList = NewsList.data.data.results
-    }
   },
   // 数据代理完成后，在此钩子获得用户关注频道
   async created () {
@@ -56,10 +51,6 @@ export default {
     console.log('用户关注的频道数据是', res)
     this.ListData = res.data.data.channels
     console.log(this.ListData)
-    // 在初次打开页面获取新闻列表推荐数据(默认渲染推荐列表)
-    const NewsList = await GetNewsList(GetToken('token'), 0, new Date().getTime())
-    console.log('新闻推荐列表为', NewsList.data.data.results)
-    this.NewsList = NewsList.data.data.results
   }
 }
 </script>
